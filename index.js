c9c4bdef9b6362712daa47ba49729dc81a76c8f6
Cybersecurity <newboytzi@gmail.com>
2026-03-16 17:53:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -343,8 +343,8 @@ async function startQasimDev() {
                                 forwardingScore: 1,
                                 isForwarded: true,
                                 forwardedNewsletterMessageInfo: {
-                                    newsletterJid: '120363319098372999@newsletter',
-                                    newsletterName: 'MEGA MD',
+                                    newsletterJid: '120363387807166164@newsletter',
+                                    newsletterName: 'SASAMPA-MD',
                                     serverMessageId: -1
                                 }
                             }
@@ -486,8 +486,8 @@ async function startQasimDev() {
                             forwardingScore: 1,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
-                                newsletterJid: '120363319098372999@newsletter',
-                                newsletterName: 'MEGA MD',
+                                newsletterJid: '120363387807166164@newsletter',
+                                newsletterName: 'SASAMPA-MD',
                                 serverMessageId: -1
                             }
                         }
