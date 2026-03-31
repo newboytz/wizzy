be11e7306aad956fcc6c42aed4f4f5093d74d166
Cybersecurity <newboytzi@gmail.com>
2026-03-31 12:54:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const settings = {
   // Array fallback: splits string by comma, or uses default array
   prefixes: process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.', '!', '/', '#'],
-  
+
+  wizzyBot: process.env.WIZZY_BOT || 'SASAPMA-MD',
   packname: process.env.PACKNAME || 'SASAMPA-MD',
   author: process.env.AUTHOR || 'wizzy',
   timeZone: process.env.TIMEZONE || 'Africa/Dar_es_Salaam',
